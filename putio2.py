f7992b540d404f2270c8c2bfbec6251981a16000
Cenk Alti <cenkalti@gmail.com>
2012-01-19 12:54:04 +0200

    -

--- putio2.py
+++ putio2.py
@@ -4,15 +4,23 @@ A python wrapper for put.io APIv2
 
 https://github.com/putdotio/putio-apiv2-python
 
+
+Usage:
+
+# TODO write usage
+
 """
 
 import json
+import logging
 import requests
 from urllib import urlencode
 
+logger = logging.getLogger(__name__)
+
 API_URL = 'https://put.io/v2'
-ACCESS_TOKEN_URL = 'https://api.put.io/v2/oauth2/access_token'
-AUTHENTICATION_URL = 'https://api.put.io/v2/oauth2/authenticate'
+ACCESS_TOKEN_URL = 'https://put.io/v2/oauth2/access_token'
+AUTHENTICATION_URL = 'https://put.io/v2/oauth2/authenticate'
 
 
 class AuthHelper(object):
@@ -41,28 +49,59 @@ class AuthHelper(object):
         r = requests.get(ACCESS_TOKEN_URL, params=params)
         d = json.loads(r.content)
         return d['access_token']
-    
-    
+
+
 class Client(object):
     def __init__(self, access_token):
         self.access_token = access_token
-                
-    def request(self, method='GET', path, params=None, data=None):
+        
+        attributes = {'client': self}
+        self.File = type('File', (_File,), attributes)
+        #self.Transfer = type('Transfer', (_Transfer,), attributes)
+    
+    def request(self, path, method='GET', params=None, data=None, as_dict=True):
         if not params:
             params = {}
         params['oauth_token'] = self.access_token
         
         url = API_URL + path
-        print 'url:', url
-        r = requests.request(method, url, params=params, data=data)
-        print r; print r.content
-        return json.loads(r.content)
+        logger.debug('url: %s', url)
+        r = requests.request(method, url, params=params, data=data, allow_redirects=True)
+        logger.debug('response: %s', r)
+        if not as_dict:
+            return r.content
+        
+        logger.debug('content: %s', r.content)
+        r = json.loads(r.content)
+        if r['status'] == 'ERROR':
+            raise Exception(r['error_type'])
+        return r
+
+
+class _BaseResource(object):
+    def __init__(self, resource_dict):
+        self.__dict__.update(resource_dict)
+        
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        # shorten name for display
+        name = self.name[:17] + '...' if len(self.name) > 20 else self.name
+        return 'File(%s, "%s")' % (self.id, name)
+
+
+class _File(_BaseResource):
+    @classmethod
+    def list(cls, parent_id=0):
+        d = cls.client.request('/files/list', params=dict(parent_id=parent_id))
+        files = d['files']
+        files = [cls(f) for f in files]
+        ids = [f.id for f in files]
+        return dict(zip(ids, files))
     
-    def files_list(self, parent_id=0):
-        return self.request('/files/list', params=dict(parent_id=parent_id))
-    
-    def files_download(self, file_id=0):
-        return self.request('/files/%s' % file_id)
-    
-    def files_delete(self, file_id=0):
-        return self.request('/files/%s/delete' % file_id)
+    def download(self):
+        return self.client.request('/files/%s' % self.id, as_dict=False)
+
+    def delete(self):
+        return self.client.request('/files/%s/delete' % self.id)
