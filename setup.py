2b0c8619ff6e39ed7402e4868250ef39dee6890c
Cenk Alti <cenkalti@gmail.com>
2015-11-17 13:23:49 +0200

    bump version to 2.3.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='putio.py',
-    version='2.3.3',
+    version='2.3.4',
     author=u'Cenk Altı',
     author_email='cenkalti@gmail.com',
     url='http://github.com/cenkalti/putio.py',
