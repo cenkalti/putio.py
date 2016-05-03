f357f83ccb8f533d2d963d298030334659c11ea1
Cenk Alti <cenkalti@gmail.com>
2016-05-03 13:44:36 +0300

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='putio.py',
-    version='2.4.2',
+    version='2.4.3',
     author=u'Cenk Altı',
     author_email='cenkalti@gmail.com',
     url='http://github.com/cenkalti/putio.py',
