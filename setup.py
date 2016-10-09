d6101f59e9e7860647ed916d800895c69f809d8d
Cenk Alti <cenkalti@gmail.com>
2016-10-09 17:23:50 +0300

    bump minor version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='putio.py',
     description='Python client for put.io API v2',
-    version='3.1.2',
+    version='3.2.0',
     author=u'Cenk Altı',
     author_email='cenkalti@gmail.com',
     url='http://github.com/cenk/putio.py',
