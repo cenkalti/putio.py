b861f0576d231cdb70dfac8e71cbb7b815def072
Cenk Alti <cenkalti@gmail.com>
2017-03-11 10:01:59 +0300

    bump version

--- putiopy.py
+++ putiopy.py
@@ -15,7 +15,7 @@ import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
-__version__ = '6.0.0'
+__version__ = '6.1.0'
 
 KB = 1024
 MB = 1024 * KB
