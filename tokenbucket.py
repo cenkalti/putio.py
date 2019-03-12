081a59da3c4de10f95e54f129cef5d71638af8dd
Jamie Begin <jjbegin@gmail.com>
2019-03-12 19:06:21 -0400

    Added __init__ and tokenbucket files

---
+++ tokenbucket.py
@@ -0,0 +1,38 @@
+from time import time
+from threading import Lock
+
+
+class TokenBucket:
+    """
+    An implementation of the token bucket algorithm.
+    """
+    def __init__(self):
+        self.tokens = 0
+        self.rate = 0
+        self.last = time()
+        self.lock = Lock()
+
+    def set_rate(self, rate):
+        with self.lock:
+            self.rate = rate
+            self.tokens = self.rate
+
+    def consume(self, tokens):
+        with self.lock:
+            if not self.rate:
+                return 0
+
+            now = time()
+            lapse = now - self.last
+            self.last = now
+            self.tokens += lapse * self.rate
+
+            if self.tokens > self.rate:
+                self.tokens = self.rate
+
+            self.tokens -= tokens
+
+            if self.tokens >= 0:
+                return 0
+            else:
+                return -self.tokens / self.rate
